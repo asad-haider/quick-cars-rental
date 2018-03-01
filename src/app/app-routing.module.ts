3dbbee9b4d31904aa623d6489954d1e1fc990b38
asad.haider13 <asad.haider13@gmail.com>
2018-03-01 12:25:14 +0500

    all pages integrated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,15 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AboutUsComponent} from './aboutus/aboutus.component';
 import {HomeComponent} from './home/home.component';
+import {ListingComponent} from './listing/listing.component';
+import {NewsComponent} from './news/news.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+import {HowItWorksComponent} from './how-it-works/how-it-works.component';
+import {ServicesComponent} from './services/services.component';
+import {ListingDetailsComponent} from './listing-details/listing-details.component';
+import {NewsDetailsComponent} from './news-details/news-details.component';
+import {RegisterComponent} from './register/register.component';
+import {ReservationComponent} from './reservation/reservation.component';
 
 const routes: Routes = [
   {
@@ -10,18 +19,67 @@ const routes: Routes = [
       {
         path: '',
         component: HomeComponent,
+        pathMatch: 'full',
       },
       {
         path: 'about-us',
         component: AboutUsComponent,
-      }
+      },
+      {
+        path: 'car-listings',
+        children: [
+          {
+            path: '',
+            component: ListingComponent,
+            pathMatch: 'full',
+          },
+          {
+            path: 'details',
+            component: ListingDetailsComponent,
+          }
+        ]
+      },
+      {
+        path: 'how-it-works',
+        component: HowItWorksComponent,
+      },
+      {
+        path: 'news',
+        children: [
+          {
+            path: '',
+            component: NewsComponent,
+            pathMatch: 'full',
+          },
+          {
+            path: 'details',
+            component: NewsDetailsComponent,
+          }
+        ]
+      },
+      {
+        path: 'contact-us',
+        component: ContactUsComponent,
+      },
+      {
+        path: 'services',
+        component: ServicesComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: 'reservation',
+        component: ReservationComponent,
+      },
     ]
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 
 export class AppRoutingModule {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
-import { AboutUsComponent } from './aboutus/aboutus.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
+import {AboutUsComponent} from './aboutus/aboutus.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HomeComponent} from './home/home.component';
+import {ListingComponent} from './listing/listing.component';
+import {HowItWorksComponent} from './how-it-works/how-it-works.component';
+import {NewsComponent} from './news/news.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+import {ServicesComponent} from './services/services.component';
+import {ListingDetailsComponent} from './listing-details/listing-details.component';
+import { NewsDetailsComponent } from './news-details/news-details.component';
+import { RegisterComponent } from './register/register.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 
 @NgModule({
@@ -16,14 +25,24 @@ import { HomeComponent } from './home/home.component';
     HeaderComponent,
     FooterComponent,
     AboutUsComponent,
-    HomeComponent
+    HomeComponent,
+    ListingComponent,
+    HowItWorksComponent,
+    NewsComponent,
+    ContactUsComponent,
+    ServicesComponent,
+    ListingDetailsComponent,
+    NewsDetailsComponent,
+    RegisterComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
 export class AppModule {
 }
